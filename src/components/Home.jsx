4dbc6d2db9d98dbd2467eeb2174c92846b1c0511
Shivansh Pathak <128770733+shivanshpathak01@users.noreply.github.com>
2025-04-01 08:03:21 +0000

    usestate added and props included

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ImagePreview from './ImagePreview'
 import ImageUpload from './ImageUpload'
 
+//29:18
 function Home() {
   const [uploadImage, setUploadImage] = useState(null)
   const [enhancedImage, setEnhancedImage] = useState(null)
+  const [loading, setloading] = useState(false)
+
+
+  const UploadImagehandler = (file) =>{} 
+
   return (
     <>
       <ImageUpload/>
-      <ImagePreview/>
+      <ImagePreview 
+      loading={loading} 
+      upload={uploadImage}
+      enhanced={enhancedImage}
+      
+      />
     </>
   )
 }
