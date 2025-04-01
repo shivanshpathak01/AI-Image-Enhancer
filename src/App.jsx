b50a06e5ee7688074ce053c8cdcc70a21ec4c0b4
Shivansh Pathak <128770733+shivanshpathak01@users.noreply.github.com>
2025-04-01 07:57:10 +0000

    Added changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100 py-8 px-4'>
       <div className='text-center mb-8'>
-        <h1 className='text-5xl fond-bold text-gray-800 mb-2'>AI Image Enhancer</h1>
+        <h1 className='text-5xl font-bold text-gray-800 mb-2'>AI Image Enhancer</h1>
         <p className='text-lg text-gray-500'>Upload your images here and let AI enhance it for you in seconds!</p>
       </div>
       <Home />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,11 +3,13 @@ import ImagePreview from './ImagePreview'
 import ImageUpload from './ImageUpload'
 
 function Home() {
+  const [uploadImage, setUploadImage] = useState(null)
+  const [enhancedImage, setEnhancedImage] = useState(null)
   return (
-    <div className=''>
-      <ImagePreview/>
+    <>
       <ImageUpload/>
-    </div>
+      <ImagePreview/>
+    </>
   )
 }
 
