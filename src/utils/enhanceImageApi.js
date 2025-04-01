c41043093853d315b15638961d08ca083275972f
Shivansh Pathak <128770733+shivanshpathak01@users.noreply.github.com>
2025-04-01 17:35:53 +0000

    initial commit of this JS file

---
+++ src/utils/enhanceImageApi.js
@@ -0,0 +1,14 @@
+const API_KEY = import.meta.env.VITE_PICWISH_KEY
+
+const BASE_URL = "https://techhk.aoscdn.com/"
+export const enhancedImageAPI = async (file) => {
+    try {
+        // Code to Upload image
+
+        
+
+        // fetch enhanced image
+    } catch (error) {
+        console.log("Error Enhancing Image",error.message)
+    }
+}
