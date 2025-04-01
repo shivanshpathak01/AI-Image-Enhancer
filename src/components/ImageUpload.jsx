b73fad277f4937840a74e2df088824c6db647c33
Shivansh Pathak <128770733+shivanshpathak01@users.noreply.github.com>
2025-04-01 06:44:07 +0000

    little changes in ImageUpload

--- src/components/ImageUpload.jsx
+++ src/components/ImageUpload.jsx
@@ -5,7 +5,7 @@ function ImageUpload() {
     <div className='bg-white shadow-lg rounded-2xl p-6 w-full max-w-2xl'>
       <label htmlFor="fileInput" className='block w-full cursor-pointer border-2 border-dashed border-gray-300 rounded-lg p-6 text-center hover:border-blue-500 transition-all'>
         <input type="file" id='fileInput' className='hidden'/>
-        <p>Click and drag to Upload</p>
+        <span className='text-lg font-medium text-gray-600'>Click and drag to Upload</span>
       </label>
     </div>
   )
