326b2a21bb6125477c9274068c975035950b2826
Shivansh Pathak <128770733+shivanshpathak01@users.noreply.github.com>
2025-04-01 17:35:04 +0000

    Loader made

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Loading() {
   return (
-    <div>Loading</div>
+    <div className='flex justify-center items-center h-full '>
+      <div className='animate-spin border-4 border-t-transparent w-10 h-10 scale-200 rounded-full border-red-500 mb-12'>
+
+      </div>
+    </div>
   )
 }
 
