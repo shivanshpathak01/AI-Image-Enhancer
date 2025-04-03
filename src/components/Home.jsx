9cb942ec915360ebfd06fdc9ed0bb8d74eab5f9d
Shivansh Pathak <shivanshpathak0001@gmail.com>
2025-04-03 11:32:56 +0530

    Dark mode added and UI Updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,11 +3,10 @@ import ImagePreview from './ImagePreview'
 import ImageUpload from './ImageUpload'
 import { enhancedImageAPI } from "../utils/enhanceImageApi" 
 
-function Home() {
+function Home({ darkMode }) {
   const [uploadImage, setUploadImage] = useState(null)
   const [enhancedImage, setEnhancedImage] = useState(null)
   const [loading, setloading] = useState(false)
-
 
   const UploadImagehandler = async(file) =>{
     setUploadImage(URL.createObjectURL(file));
@@ -22,17 +21,24 @@ function Home() {
     }
   } 
 
-
   return (
-    <>
-      <ImageUpload UploadImagehandler={UploadImagehandler}/>
-      <ImagePreview 
-      loading={loading} 
-      upload={uploadImage}
-      enhanced={enhancedImage?.image}
-      
-      />
-    </>
+    <div className={`w-full max-w-6xl mx-auto backdrop-blur-lg ${
+      darkMode ? 'bg-white/10' : 'bg-white/80'
+    } rounded-3xl p-8 shadow-2xl transition-all duration-300`}>
+      <div className="max-w-6xl mx-auto space-y-8">
+        {/* <div className="text-center mb-5">
+          
+        </div> */}
+        
+        <ImageUpload UploadImagehandler={UploadImagehandler} darkMode={darkMode} />
+        <ImagePreview 
+          loading={loading} 
+          upload={uploadImage}
+          enhanced={enhancedImage?.image}
+          darkMode={darkMode}
+        />
+      </div>
+    </div>
   )
 }
 
